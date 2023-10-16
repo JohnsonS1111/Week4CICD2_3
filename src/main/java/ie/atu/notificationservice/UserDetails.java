aeb56d77ac0bf8dcc0b346c0c10ef5fdf9acd7ea
Johnson <g00389668@atu.ie>
2023-10-17 00:43:05 +0100

    new notification application openfeign connection

---
+++ src/main/java/ie/atu/notificationservice/UserDetails.java
@@ -0,0 +1,13 @@
+package ie.atu.notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    private String email;
+    private String name;
+}
